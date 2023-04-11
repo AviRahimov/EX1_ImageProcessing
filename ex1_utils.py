a7fe59e329e1fd874a8c2b037e35515d66a30abf
Avi Rahimov <rahimovavi3@gmail.com>
2023-04-11 13:40:49 +0300

    finish all functions in ex1.utils, need to finish the gamma function

--- ex1_utils.py
+++ ex1_utils.py
@@ -8,10 +8,13 @@
          ########: ##:::. ##::'######:
         ........::..:::::..:::......::
 """
+import math
 from typing import List
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import mean_squared_error as MSE
+from sklearn.cluster import KMeans
 
 LOAD_GRAY_SCALE = 1
 LOAD_RGB = 2
@@ -63,20 +66,13 @@ def imDisplay(filename: str, representation: int):
     elif representation == 2:
         plt.imshow(img_arr)
         plt.show()
-    pass
 
-
-# def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
-#     """
-#     Converts an RGB image to YIQ color space
-#     :param imgRGB: An Image in RGB
-#     :return: A YIQ in image color space
-#     """
-#     imgRGB_mat = imgRGB.reshape(-1, 3)
-#     mat_mult = np.array(np.dot(imgRGB_mat, chromatic_comp_mat))
-#     YIQ_mat = mat_mult.reshape(imgRGB.shape)
-#     return YIQ_mat
 def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
+    """
+    Converts an RGB image to YIQ color space
+    :param imgRGB: An Image in RGB
+    :return: A YIQ in image color space
+    """
     if len(imgRGB.shape) == 2:
         return
     # Use np.einsum() to perform matrix multiplication without flattening and reshaping
@@ -85,18 +81,12 @@ def transformRGB2YIQ(imgRGB: np.ndarray) -> np.ndarray:
     # Return the output image
     return imgYIQ
 
-# def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
-#     """
-#     Converts an YIQ image to RGB color space
-#     :param imgYIQ: An Image in YIQ
-#     :return: A RGB in image color space
-#     """
-#     inverse_chromatic_comp_mat = np.linalg.inv(chromatic_comp_mat)
-#     imgYIQ_mat = imgYIQ.reshape(-1, 3)
-#     res_mat = np.array(np.dot(imgYIQ_mat, inverse_chromatic_comp_mat))
-#     RGB_mat = res_mat.reshape(imgYIQ.shape)
-#     return RGB_mat
 def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
+    """
+    Converts an YIQ image to RGB color space
+    :param imgYIQ: An Image in YIQ
+    :return: A RGB in image color space
+    """
     # Define the matrix for converting YIQ to RGB
     inverse_chromatic_comp_mat = np.linalg.inv(chromatic_comp_mat)
     # Get the shape of the input image
@@ -105,14 +95,15 @@ def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
     # Return the output image
     return imgRGB
 
+
 def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarray):
     """
     Equalizes the histogram of an image
     :param imgOrig: Original Histogram
     :return: (imgEq,histOrg,histEQ)
     """
-    Origimg = 0
-    YIQimg = 0
+    Origimg = imgOrig.copy()
+    YIQimg = imgOrig.copy()
     img_len = len(imgOrig.shape)
     # imgOrig is a RGB image
     if img_len == 3:
@@ -125,7 +116,6 @@ def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarra
         Origimg = imgOrig
 
     # stretching the original image because the original image is between the range [0,1]
-    # stretched_imgOrg = np.ndarray(Origimg*255).astype('uint8')
     stretched_imgOrg = cv2.normalize(Origimg, None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
     # calculating the histogram of the original image with 256 bins and take the first argument([0]) that is the data
     hist_imgOrg = np.histogram(stretched_imgOrg, bins=256)[0]
@@ -150,6 +140,7 @@ def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarra
 
     return imgEq, hist_imgOrg, hist_imgEq
 
+
 def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarray], List[float]):
     """
     Quantized an image in to **nQuant** colors
@@ -158,4 +149,102 @@ def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarr
     :param nIter: Number of optimization loops
     :return: (List[qImage_i],List[error_i])
     """
-    pass
+    # List of the quantized image in each iteration.
+    qImage = []
+    # List of the MSE error in each iteration.
+    error = []
+    # The borders which divide the histograms into segments, size of z = nQuant+1.
+    z = []
+    Origimg = 0
+    YIQimg = 0
+    Quantimg = 0
+    img_len = len(imOrig.shape)
+
+    # imgOrig is a RGB image
+    if img_len == 3:
+        # transform the image to YIQ image and taking only the Y channel.
+        YIQimg = transformRGB2YIQ(imOrig)
+        Origimg = YIQimg[:, :, 0]
+    # imgOrig is a grayscale image
+    elif img_len == 2:
+        # if the image is already RGB image so we leave it like that
+        Origimg = imOrig
+
+    # stretching the original image because the original image is between the range [0,1]
+    stretched_imgOrg = cv2.normalize(Origimg, None, 0, 255, cv2.NORM_MINMAX).astype('uint8')
+    copy_img = stretched_imgOrg.copy()
+    imgOrig_hist = np.histogram(stretched_imgOrg.flatten(), bins=256)[0]
+    # Computing the cumulative sum for the image for finding the first boundaries
+    imgCumSum = np.cumsum(imgOrig_hist)
+    z.append(0)
+    # first boundary will be the number of pixels dividing by nQuant as writing in assignment PDF that we need to set
+    # initial segment division
+    Quant = imgCumSum[-1] / nQuant
+    # setting the rest of the boundaries
+    for boundary in range(1, nQuant):
+        z.append(np.where(imgCumSum >= boundary * Quant)[0][0])
+    z.append(255)
+
+    for iteration in range(nIter):
+        # The values that each of the segments’ intensities will map to.
+        q = []
+        # calculating mean for each segment(q)
+        for i in range(nQuant):
+            mean = np.mean(stretched_imgOrg[(stretched_imgOrg > z[i]) & (stretched_imgOrg <= z[i + 1])])
+            q.append(mean)
+
+        # Create the quantize image
+        Quantimg = np.zeros_like(copy_img)
+        for i in range(nQuant):
+            Quantimg[(stretched_imgOrg > z[i]) & (stretched_imgOrg <= z[i + 1])] = q[i]
+
+        # Calculating new values(fixed values) for z
+        z.clear()
+        z.append(0)
+        for i in range(len(q) - 1):
+            z.append(int((q[i] + q[i + 1]) / 2))
+        z.append(255)
+        MSE_error = MSE(Origimg*255, Quantimg)
+        error.append(MSE_error)
+        if iteration > 5:
+            if check_MSE_convergence(error, math.pow(10, -5)):
+                break
+        qImage.append(Quantimg/255.0)
+    # if the image is colored so we need to convert it back to RGB since we worked on the Y channel
+    if img_len == 3:
+        for i in range(len(qImage)):
+            YIQimg[:, :, 0] = qImage[i]
+            qImage[i] = transformYIQ2RGB(YIQimg)
+
+    return qImage, error
+
+
+def check_MSE_convergence(MSE_list: list, tolerance: float) -> bool:
+    # checking if the difference between the last 10 numbers is less than 10^-5
+    for i in range(-6, -1):
+        if (MSE_list[i] - MSE_list[i + 1]) > tolerance:
+            return False
+    return True
+
+# Kmeans implementation for image quantization
+# def quantizeImage(img, num_colors, num_iterations):
+#     # Flatten the image into a 2D array of pixels
+#     pixels = img.reshape((-1, 3))
+#
+#     # Perform K-means clustering to determine the centroids of the color clusters
+#     kmeans = KMeans(n_clusters=num_colors, max_iter=num_iterations).fit(pixels)
+#     centroid_colors = kmeans.cluster_centers_
+#
+#     # Quantize the image by replacing each pixel with the nearest centroid color
+#     quantized_pixels = np.zeros_like(pixels)
+#     for i in range(num_colors):
+#         quantized_pixels[kmeans.labels_ == i] = centroid_colors[i]
+#
+#     # Reshape the quantized pixel array back into an image
+#     quantized_img = quantized_pixels.reshape(img.shape)
+#
+#     # Compute the mean squared error between the original and quantized images
+#     mse = np.mean((img - quantized_img) ** 2)
+#
+#     # Return the quantized image and the MSE
+#     return [quantized_img], [mse]
