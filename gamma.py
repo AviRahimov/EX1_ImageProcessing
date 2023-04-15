e16d3cb0b3719732ab71b81295e7ec5c8a0c3b8e
Avi Rahimov <rahimovavi3@gmail.com>
2023-04-16 00:15:41 +0300

    Fix the gamma correction function

--- gamma.py
+++ gamma.py
@@ -8,15 +8,14 @@
          ########: ##:::. ##::'######:
         ........::..:::::..:::......::
 """
-# import ex1_utils
 import sys
-
 import numpy as np
+from ex1_utils import imReadAndConvert
 from ex1_utils import LOAD_GRAY_SCALE
 import cv2
 from cv2 import createTrackbar
 
-slider_max_val = 100
+slider_max_val = 200
 window_name = "Gamma Correction GUI presentation"
 
 
@@ -28,41 +27,17 @@ def gammaDisplay(img_path: str, rep: int):
     :return: None
     """
     global img
-
-    if img_path is None:
-        raise Exception("The file you provided isn't exist, pls try different file path")
-    # The image is in grayscale color
-    if rep == 1:
-        img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    # The image is in RGB color
-    elif rep == 2:
-        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-    else:
-        raise Exception("You can only write rep as 0 for gray image or 1 for RGB image")
-
+    img = imReadAndConvert(img_path, rep)
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
     trackbar_name = f"Gamma "
     createTrackbar(trackbar_name, window_name, 50, slider_max_val, on_trackbar)
-    on_trackbar(1)
     cv2.waitKey()
     cv2.destroyAllWindows()
     sys.exit()
 
 def on_trackbar(val):
-
-    # Convert the integer trackbar value to a floating-point gamma value, and set gamma to be 0.001(arbitrary) because
-    # if we allow gamma to be 0 we will get an error for dividing by zero
-    gamma = val / slider_max_val if val != 0 else 0.001
-
-    # Create a look-up table for gamma correction
-    inv_gamma = 1.0 / gamma
-    table = np.array([((i / 255.0) ** inv_gamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
-
-    # The formula assumes that the pixel values in the image are in the range [0, 255], but in reality,the pixel
-    # values may be outside this range so, we need to create a look-up table to adjust the pixels in the range [0,255].
-    gamma_corrected = cv2.LUT(img, table)
-
-    # Display the gamma corrected image
+    gamma = val / (slider_max_val/2)
+    gamma_corrected = np.power(img, gamma)
     cv2.imshow(window_name, gamma_corrected)
 
 def main():
